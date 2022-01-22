976a5ad273573b3d7b742a4af665e9ced81f82ca
Yaroslav_Holovan <yaroslav.golovan@gmail.com>
2022-01-22 10:45:06 +0200

    aee

--- src/Game.java
+++ src/Game.java
@@ -2,36 +2,27 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
+@SuppressWarnings("ALL")
 public class Game {
+
     public static void main(String[] args) throws IOException {
 
-        Random random = new Random(); //енератор рандомных чисел
-        List<String> arr = new ArrayList<>(); //список
-        File file = new File("src/Files/five_letters_ukr.txt"); // создание объекта "Файл" из текстово файла
-        FileReader fr = new FileReader(file); // читалка для файла
 
-        try (BufferedReader br = new BufferedReader(fr)) // пробуем читать файл(если файла нет, отлавливаем ексепшн)
-        {
-            String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) { //читаем по 1-й строке
-                arr.add(sCurrentLine);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        fr.close();
-        Map<String, String> map = arr.stream().collect(Collectors.toMap(x -> x, x -> x)); //создаем стримом мапу из списка, где ключ это слово, а значение то же самое слово из ключа
+        Map<String, String> map = readFileToMap("src/Files/five_letters_ukr.txt");
+        String randomWord = generateRandom(map);
+
         Scanner scanner = new Scanner(System.in);
-        String randomWord = arr.get(random.nextInt(arr.size() - 1)).toLowerCase(Locale.ROOT); //выбор рандомного слова и переводим в нижний регистр
-
+        //String randomWord = map.get(random.nextInt(arr.size() - 1)).toLowerCase(Locale.ROOT); //выбор рандомного слова и переводим в нижний регистр
+        System.out.println(randomWord);
         for(int i = 0; i < 6; i++) { // 6 попыток
+            System.out.println("Спроба №" + (i + 1));
             String str = scanner.nextLine().toLowerCase(Locale.ROOT); // сканируем слово и переводив в нижний регистр
             if (map.get(str) == null) { // если введенное слово не из словаря
                 System.out.println("Слова немає в словнику");
                 i--;
             }
             else if (str.equals(randomWord)) { //если введенное слово совпалоо с рандомным:
-                System.out.println("You win!");
+                System.out.println("Ви перемогли! Слава Україні!");
                 break;
             }
             else { // проверяем, совпали ли какие-то буквы
@@ -53,11 +44,34 @@ public class Game {
                 System.out.println();
 
                 if (i == 5) {
-                    System.out.println("Лошара, ты проиграл, загаданное слово: " + randomWord.toUpperCase(Locale.ROOT));
+                    System.out.println("Ви програли, загадане слово: " + randomWord.toUpperCase(Locale.ROOT));
                 }
             }
         }
     scanner.close();
 
+    }
+
+    public static String generateRandom(Map<String, String> map){
+        Random random = new Random(); //енератор рандомных чисел
+        return map.values().stream().collect(Collectors.toList()).get(random.nextInt(map.size()));
+    }
+
+    public static Map<String, String> readFileToMap(String path) throws IOException {
+        Map<String, String> map = new HashMap<>(); //список
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))) // пробуем читать файл(если файла нет, отлавливаем ексепшн)
+        {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) { //читаем по 1-й строке
+                map.put(sCurrentLine, sCurrentLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map; //создаем стримом мапу из списка, где ключ это слово, а значение то же самое слово из ключа
+    }
+
+    public String check(String randomWord, String scannedWord){
+        return null;
     }
 }
